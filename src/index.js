62adf2fc78676bfba59786fa1cd5fbed49beb150
hezijian <2322553132@qq.com>
2024-04-18 14:23:42 +0800

    the first commint

---
+++ src/index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+
+const package = require('../package.json');
+
+// 获取package.version
+const { version } = package;
+const program = new Command();
+
+// 定义当前版本
+program
+  .version(
+    `simptk: ${version}`,
+    '-v, --version',
+    'output the current pri version',
+  )
+  .command('create')
+  .action((name) => {});
+
+// 解析运行参数(必须且要放在最后一行)
+program.parse(process.argv);
